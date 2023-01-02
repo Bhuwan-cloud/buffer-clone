fc34fc2a5239b77da1bd301b2a87b1f222e97578
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2023-01-02 18:14:59 +0545

    validations written to user mdoel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # password,  password_confirmation = virtual field
 class User < ApplicationRecord
-  has_secure_password :password
+  has_secure_password()
+  before_save { |user| user.email = email.downcase }
+  validates(:email, uniqueness: { case_sensitive: false }, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" })
+  validates(:password, presence: true, length: { in: 6..20 }) if new?
+  validates(:password_confirmation, presence: true) if new?
 end
